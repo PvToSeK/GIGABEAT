65f46af32307330b5ae16dadb942c6379d75d602
PvToSeK <marinseksamuel@gmail.com>
2026-01-28 13:22:17 +0100

    Update server.js

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const http = require ('http');
+const PORT = 6767;
+const HOSTNAME = "localhost";
+
+const heartRates = [ 
+    { patient_id: 1, bpm: 72, timestamp: "2026-01-28T10:00:00" },
+    { patient_id: 2, bpm: 75, timestamp: "2026-01-28T10:01:00" },
+    { patient_id: 3, bpm: 68, timestamp: "2026-01-28T10:00:30" } 
+];
