2ab9976f5edbcffe260bfb66c342264fe947f003
PvToSeK <marinseksamuel@gmail.com>
2026-01-28 13:42:06 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,3 +7,15 @@ const heartRates = [
     { patient_id: 2, bpm: 75, timestamp: "2026-01-28T10:01:00" },
     { patient_id: 3, bpm: 68, timestamp: "2026-01-28T10:00:30" } 
 ];
+
+const server = http.createServer((req, res) => {
+        if(req.url === "/" && req.method === "GET"){
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "text/plain");
+            return res.end("GIGABEAT backend online");
+        }
+        
+})
+server.listen(PORT, HOSTNAME, () => {
+    console.log("servizio ONLINE " + HOSTNAME + ":" + PORT);
+});
