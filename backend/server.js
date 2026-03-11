3015b42bf8a591b20af9da3068fae44e40c7708b
PvToSeK <marinseksamuel@gmail.com>
2026-03-11 13:44:21 +0100

    server.js con express

--- backend/server.js
+++ backend/server.js
@@ -1,47 +1,39 @@
-const http = require('http');
-
+const express = require('express');
 const PORT = 6767;
-const HOSTNAME = "localhost";
-
+const cors = require('cors');
+const app = express();
 const heartRates = [
     { patient_id: 1, bpm: 72, timestamp: "2026-01-28T10:00:00" },
     { patient_id: 2, bpm: 75, timestamp: "2026-01-28T10:01:00" },
     { patient_id: 3, bpm: 68, timestamp: "2026-01-28T10:00:30" }
 ];
+app.use(cors());
+app.use(express.json());
 
-const server = http.createServer((req, res) => {
+app.use((req,res,next)=>{
+    console.log(`${req.method} ${req.url} `)
+    next();
+})
 
-    if (req.url === "/" && req.method === "GET") {
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "text/plain");
-        return res.end("GIGABEAT backend online");
-    }
 
-    if (req.url === "/api/heartbeat/latest" && req.method === "GET") {
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "application/json");
-        return res.end(JSON.stringify(heartRates[heartRates.length - 1]));
-    }
 
-    if (req.url === "/api/heartbeat" && req.method === "POST") {
-        let body = '';
 
-        req.on("data", (chunk) => {
-            body += chunk;
-        });
+app.get("/api/heartbeat/all",(req,res) => {
+    res.json(heartRates); 
+})
+app.get("/api/heartbeat/latest",(req,res) => {
+    res.json(heartRates[heartRates.length - 1]); 
+})
 
-        req.on("end", () => {
-            let nuovo_battito = JSON.parse(body);
-            heartRates.push(nuovo_battito);
 
-            res.statusCode = 201;
-            res.setHeader("Content-Type", "application/json");
-            return res.end(JSON.stringify(heartRates));
-        });
-    }
 
-});
 
-server.listen(PORT, HOSTNAME, () => {
-    console.log("servizio ONLINE " + HOSTNAME + ":" + PORT);
-});
+
+
+
+
+
+
+app.listen(PORT,() =>{
+    console.log("GIGABEAT online su http://localhost:" + PORT);
+})
