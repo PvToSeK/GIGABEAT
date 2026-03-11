2c2a92b6c6c63ded1910eaf528bbdfab40d4efa7
lidestrimiriam <lidestri.miriam@studenti.salesianisesto.it>
2026-03-11 13:01:00 +0100

    Update schema.sql

---
+++ backend/db/schema.sql
@@ -0,0 +1,52 @@
+DROP DATABASE if exists GIGABEAT;
+
+CREATE DATABASE if not exists GIGABEAT;
+USE GIGABEAT;
+
+CREATE TABLE Paziente(
+	cf_paziente int PRIMARY KEY,
+    nome varchar(100) NOT NULL,
+    cognome varchar(100) NOT NULL,
+    citta varchar(100) NOT NULL,
+    data_nascita date NOT NULL,
+    telefono int CHECK(telefono = 10),
+    contatto_emergenza int CHECK(telefono = 10)
+);
+
+CREATE TABLE Sensore(
+	id_sensore int PRIMARY KEY,
+    cf_paziente int NOT NULL,
+    
+    FOREIGN KEY (cf_paziente) REFERENCES Paziente(cf_paziente)
+);
+
+CREATE TABLE Battito(
+	id_battito int PRIMARY KEY,
+    id_sensore int NOT NULL,
+    battiti int NOT NULL,
+    data_ora datetime NOT NULL,
+    irregolare boolean,
+    
+    FOREIGN KEY (id_sensore) REFERENCES Sensore(id_sensore)
+);
+
+CREATE TABLE Posizione(
+	id_posizione int PRIMARY KEY,
+    cf_paziente int NOT NULL,
+    latitudine decimal(9, 6) NOT NULL,
+    longitudine decimal(9, 6) NOT NULL,
+    data_ora datetime NOT NULL,
+    
+    FOREIGN KEY (cf_paziente) REFERENCES Paziente(cf_paziente)
+);
+
+
+CREATE TABLE Emergenze(
+	id_emergenza int PRIMARY KEY,
+    cf_paziente int NOT NULL,
+    id_misurazione int NOT NULL,
+    data_ora datetime,
+    
+    FOREIGN KEY (cf_paziente) REFERENCES Paziente(cf_paziente),
+    FOREIGN KEY (id_battito) REFERENCES Battito(id_battito)
+);
