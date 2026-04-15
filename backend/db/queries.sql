4698f8a5b55a4abcb5acba868defacb1e8d8d83c
PvToSeK <marinseksamuel@gmail.com>
2026-04-15 13:09:23 +0200

    Refactor SQL queries for patient heart rate data

--- backend/db/queries.sql
+++ backend/db/queries.sql
@@ -2,52 +2,35 @@
     Prima versione query del database V1
 */    
     
-
-
-
-/*     
-Insert esempio INSERT INTO Battito (id_battito, id_sensore, battiti, data_ora, irregolare)
+/* Insert esempio */
+INSERT INTO Battito (id_battito, id_sensore, bpm, timestamp, irregolare)
 VALUES (1, 1, 78, NOW(), false);
 
-    Richiesta n1 : Ultimo pattito di un paziete
-*/ 
-
-SELECT b.battiti, b.data_ora
+/* Richiesta n1 : Ultimo battito di un paziente */
+SELECT b.bpm, b.timestamp
 FROM Battito b
 JOIN Sensore s ON b.id_sensore = s.id_sensore
-WHERE s.cf_paziente = 1
-ORDER BY b.data_ora DESC
+WHERE s.cf_paziente = 'CF_PAZIENTE'
+ORDER BY b.timestamp DESC
 LIMIT 1;
 
-
-
-/* Richiesta n2: Storico Battiti*/ 
-
-SELECT b.battiti, b.data_ora
+/* Richiesta n2: Storico Battiti */
+SELECT b.bpm, b.timestamp
 FROM Battito b
 JOIN Sensore s ON b.id_sensore = s.id_sensore
-WHERE s.cf_paziente = 1
-ORDER BY b.data_ora;
+WHERE s.cf_paziente = 'CF_PAZIENTE'
+ORDER BY b.timestamp;
 
-/* Richiesta n3: media Battiti*/  
-
-SELECT AVG(b.battiti) AS media_battiti
+/* Richiesta n3: Media Battiti */
+SELECT AVG(b.bpm) AS media_battiti
 FROM Battito b
 JOIN Sensore s ON b.id_sensore = s.id_sensore
-WHERE s.cf_paziente = 1;
+WHERE s.cf_paziente = 'CF_PAZIENTE';
 
-/* Richiesta n4:  Battiti irregolar */ 
-
-SELECT b.battiti, b.data_ora
+/* Richiesta n4: Battiti irregolari */
+SELECT b.bpm, b.timestamp
 FROM Battito b
 JOIN Sensore s ON b.id_sensore = s.id_sensore
-WHERE s.cf_paziente = 1
+WHERE s.cf_paziente = 'CF_PAZIENTE'
 AND b.irregolare = true;
-
-
-
-
-
-
-
 
